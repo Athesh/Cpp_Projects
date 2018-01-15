22940a105667bad00e8ebf802a6922aba0e5f4e6
Tomáš Praus <praustom@fel.cvut.cz>
2018-01-15 18:24:39 +0100

    Reading from file

--- Word_Count/Word_Count/main.cpp
+++ Word_Count/Word_Count/main.cpp
@@ -2,9 +2,23 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
+void readFileToConsole(string fileName) {
+	string line;
+	ifstream document(fileName); //ifstream = input file stream
+
+	if (document.is_open()) {
+		while (getline(document, line)) {
+			cout << line << '\n';
+		}
+		document.close();
+	} else cout << "Sorry, wasn't able to open the document :(\n";
+}
+
 int main() {
-	std::ofstream txtFile;
-	txtFile.open("document.txt");
-	txtFile << "Hey I really want to pass this semester.\n";
-	txtFile.close();
+	readFileToConsole("something.txt");
+	readFileToConsole("document.txt");
+
+	cout << "Hey I don't know how to keep my console open ... so I gotta do it through breakpoint on the end.";
 }
